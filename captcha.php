2b97862a9ae20ac226c5611a2360844ce0e9a17f
CSL2497 <118535039+CSL2497@users.noreply.github.com>
2023-04-17 09:32:47 +0800

    Create captcha.php

---
+++ captcha.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>CAPTCHA Generator</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+	
+</head>
+<body>
+	<div class="container">
+		<h1>CAPTCHA Generator</h1>
+		<img id="captcha-image" src="" alt="CAPTCHA Image">
+		<input type="text" id="captcha-input" placeholder="Enter the CAPTCHA code">
+		<button id="submit-btn" onclick="validateCaptcha()">Submit</button>
+	</div>
+	<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
